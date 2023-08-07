579ce88b06207211b679c84b5a65db1f3f1ceeb6
GuilhermeHalter <guisam.halter@gmail.com>
2023-08-07 16:45:53 -0300

    Feito tabela Modelo.

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Categoria, Marca
+from .models import Categoria, Marca, Modelo
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
+admin.site.register(Modelo)
 # Register your models here.
