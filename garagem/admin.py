801dd4105ad732d389f1214449deadbd7259bed2
GuilhermeHalter <guisam.halter@gmail.com>
2023-08-11 08:54:13 -0300

    add tabela veiculo

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from .models import Categoria, Marca, Modelo, Cor, Acessorio
+from .models import Categoria, Marca, Modelo, Cor, Acessorio, Veiculo
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
 admin.site.register(Modelo)
 admin.site.register(Cor)
 admin.site.register(Acessorio)
+admin.site.register(Veiculo)
+
 
 # Register your models here.
--- garagem/models.py
+++ garagem/models.py
@@ -29,3 +29,17 @@ class Cor (models.Model):
 class Acessorio (models.Model):
     descricao = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.descricao
+
+class Veiculo (models.Model):
+    cor = models.ForeignKey (Cor, on_delete=models.CASCADE)
+    modelo = models.ForeignKey (Modelo, on_delete=models.CASCADE)
+    ano = models.IntegerField(max_length=100)
+    descricao = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__ (self):
+        return self.descricao
+
+
