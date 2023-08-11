483e025169149ace04d05de647d6b0729d377818
GuilhermeHalter <guisam.halter@gmail.com>
2023-08-11 08:20:56 -0300

    add tabela cor

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Categoria, Marca, Modelo
+from .models import Categoria, Marca, Modelo, Cor
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
 admin.site.register(Modelo)
+admin.site.register(Cor)
 # Register your models here.
--- garagem/models.py
+++ garagem/models.py
@@ -16,3 +16,13 @@ class Modelo (models.Model):
     Categoria = models.ForeignKey (Categoria, on_delete=models.CASCADE)
     Marca = models.ForeignKey (Marca, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Cor (models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
